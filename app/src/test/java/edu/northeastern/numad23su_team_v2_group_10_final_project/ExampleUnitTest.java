f7785fa030c9276d77f1f440daaad166f3d40239
tjuqxb <tjuqxb@hotmail.com>
2023-07-18 17:35:43 -0700

    init project

---
+++ app/src/test/java/edu/northeastern/numad23su_team_v2_group_10_final_project/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package edu.northeastern.numad23su_team_v2_group_10_final_project;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}

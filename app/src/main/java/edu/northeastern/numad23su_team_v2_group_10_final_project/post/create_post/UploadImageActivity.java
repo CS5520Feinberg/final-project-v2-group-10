995ee041782bf6cb6cb8df334023a30e2fe58f93
tjuqxb <tjuqxb@hotmail.com>
2023-07-27 20:52:01 -0700

    add camera support

--- app/src/main/java/edu/northeastern/numad23su_team_v2_group_10_final_project/post/create_post/UploadImageActivity.java
+++ app/src/main/java/edu/northeastern/numad23su_team_v2_group_10_final_project/post/create_post/UploadImageActivity.java
@@ -8,10 +8,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.ActionMenuView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+import androidx.core.os.EnvironmentCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -19,6 +22,7 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
@@ -27,14 +31,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import edu.northeastern.numad23su_team_v2_group_10_final_project.R;
 
@@ -47,14 +56,21 @@ public class UploadImageActivity extends AppCompatActivity {
     MenuItem deleteItem;
     MenuItem cancelItem;
     MenuItem finishItem;
+    MenuItem cameraItem;
     Button fab;
     Boolean isInEdit = false;
 
     ImageAdapter adapter;
     ArrayList<UploadImage> list = new ArrayList<>();
 
+    private Uri mCameraUri;
+    private String mCameraImagePath;
+    private boolean isAndroidQ = Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q;
+
     private final static int PERMISSION_STORAGE = 99;
-    private final  static int IMAGE_QUEST = 100;
+    private static final int PERMISSION_CAMERA_REQUEST_CODE = 98;
+    private final static int IMAGE_QUEST = 100;
+    private final static int CAMERA_REQUEST_CODE = 101;
     final static int COL = 2;
     final static String PHOTOS = "PHOTOS";
     final static String IN_EDIT = "IN_EDIT";
@@ -67,8 +83,8 @@ public class UploadImageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_upload_image);
         if (ContextCompat.checkSelfPermission(UploadImageActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(UploadImageActivity.this,new String[]{
-                    Manifest.permission.READ_EXTERNAL_STORAGE} ,PERMISSION_STORAGE);
+            ActivityCompat.requestPermissions(UploadImageActivity.this, new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_STORAGE);
         }
         getSupportActionBar().setTitle("select photos");
         fab = findViewById(R.id.addButton);
@@ -79,6 +95,7 @@ public class UploadImageActivity extends AppCompatActivity {
         editItem = bottomAppBar.getMenu().findItem(R.id.edit);
         deleteItem = actionMenuView.getMenu().findItem(R.id.delete);
         cancelItem = bottomAppBar.getMenu().findItem(R.id.cancel);
+        cameraItem = actionMenuView.getMenu().findItem(R.id.camera);
         Bundle extras = getIntent().getExtras();
         if (extras != null && extras.containsKey(PHOTOS)) {
             list.clear();
@@ -93,11 +110,11 @@ public class UploadImageActivity extends AppCompatActivity {
                 isInEdit = savedInstanceState.getBoolean(IN_EDIT);
             }
         }
-        for (int i = 0;i < list.size(); i++) {
+        for (int i = 0; i < list.size(); i++) {
             list.get(i).width = width;
         }
         switchMode();
-        adapter = new ImageAdapter(this,list);
+        adapter = new ImageAdapter(this, list);
         ImageClickListener clickListener = new ImageClickListener() {
             @Override
             public void onClick(int position) {
@@ -152,6 +169,14 @@ public class UploadImageActivity extends AppCompatActivity {
             }
         });
 
+        cameraItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(@NonNull MenuItem menuItem) {
+                checkPermissionAndCamera();
+                return true;
+            }
+        });
+
         recyclerView.post(new Runnable() {
             @Override
             public void run() {
@@ -190,9 +215,10 @@ public class UploadImageActivity extends AppCompatActivity {
             fab.setVisibility(View.VISIBLE);
         }
         editItem.setVisible(!isInEdit);
+        cameraItem.setVisible(!isInEdit);
         deleteItem.setVisible(isInEdit);
         cancelItem.setVisible(isInEdit);
-        if (finishItem != null)  finishItem.setVisible(!isInEdit);
+        if (finishItem != null) finishItem.setVisible(!isInEdit);
     }
 
     private void selectImage() {
@@ -214,9 +240,17 @@ public class UploadImageActivity extends AppCompatActivity {
             int count = data.getClipData().getItemCount();
             for (int i = 0; i < count; i++) {
                 Uri imageUri = data.getClipData().getItemAt(i).getUri();
-                list.add (new UploadImage(imageUri.toString(), getPath(this, imageUri), width / 2));
+                list.add(new UploadImage(imageUri.toString(), getPath(this, imageUri), width / 2));
             }
             adapter.notifyItemRangeInserted(list.size() - count, count);
+        } else if (requestCode == CAMERA_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                ViewGroup parent = (ViewGroup) recyclerView.getParent();
+                width = parent.getWidth();
+                list.add(new UploadImage(mCameraUri.toString(), getPath(this, mCameraUri), width / 2));
+            } else {
+                Toast.makeText(this, "Cancel", Toast.LENGTH_LONG).show();
+            }
         }
     }
 
@@ -236,5 +270,85 @@ public class UploadImageActivity extends AppCompatActivity {
         dt.putParcelableArrayListExtra("PHOTOS", list);
         setResult(RESULT_OK, dt);
         this.finish();
+    }
+
+    // camera ref: https://juejin.cn/post/6844903944095793166
+    private void checkPermissionAndCamera() {
+        int hasCameraPermission = ContextCompat.checkSelfPermission(getApplication(),
+                Manifest.permission.CAMERA);
+        if (hasCameraPermission == PackageManager.PERMISSION_GRANTED) {
+            openCamera();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    PERMISSION_CAMERA_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_CAMERA_REQUEST_CODE) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera();
+            } else {
+                Toast.makeText(this, "Need camera permission.", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+    private void openCamera() {
+        Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        File photoFile = null;
+        Uri photoUri = null;
+        if (isAndroidQ) {
+            photoUri = createImageUri();
+        } else {
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (photoFile != null) {
+                mCameraImagePath = photoFile.getAbsolutePath();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    photoUri = FileProvider.getUriForFile(this, getPackageName() + ".fileprovider", photoFile);
+                } else {
+                    photoUri = Uri.fromFile(photoFile);
+                }
+            }
+        }
+
+        mCameraUri = photoUri;
+        if (photoUri != null) {
+            captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+            captureIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            startActivityForResult(captureIntent, CAMERA_REQUEST_CODE);
+        }
+
+    }
+
+    private Uri createImageUri() {
+        String status = Environment.getExternalStorageState();
+        if (status.equals(Environment.MEDIA_MOUNTED)) {
+            return getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, new ContentValues());
+        } else {
+            return getContentResolver().insert(MediaStore.Images.Media.INTERNAL_CONTENT_URI, new ContentValues());
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String imageName = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        if (!storageDir.exists()) {
+            storageDir.mkdir();
+        }
+        File tempFile = new File(storageDir, imageName);
+        if (!Environment.MEDIA_MOUNTED.equals(EnvironmentCompat.getStorageState(tempFile))) {
+            return null;
+        }
+        return tempFile;
     }
 }

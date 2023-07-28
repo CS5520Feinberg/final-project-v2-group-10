e66c2b677cd5ac4144ef8f965714cb1b2ee6f373
tjuqxb <tjuqxb@hotmail.com>
2023-07-27 21:25:07 -0700

    delete unused camera file

--- app/src/main/java/edu/northeastern/numad23su_team_v2_group_10_final_project/post/create_post/UploadImageActivity.java
+++ app/src/main/java/edu/northeastern/numad23su_team_v2_group_10_final_project/post/create_post/UploadImageActivity.java
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
+import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
@@ -250,6 +251,8 @@ public class UploadImageActivity extends AppCompatActivity {
                 list.add(new UploadImage(mCameraUri.toString(), getPath(this, mCameraUri), width / 2));
             } else {
                 Toast.makeText(this, "Cancel", Toast.LENGTH_LONG).show();
+                ContentResolver contentResolver = getContentResolver ();
+                contentResolver.delete (mCameraUri,null ,null );
             }
         }
     }

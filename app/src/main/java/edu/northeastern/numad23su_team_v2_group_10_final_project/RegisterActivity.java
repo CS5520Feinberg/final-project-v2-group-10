ac1e1a431f36928961479225db3a73958b4ae5fc
Jingjing Liao <jingjingliao@Jingjings-MacBook-Pro-New.local>
2023-08-03 22:35:09 -0700

    update image

--- app/src/main/java/edu/northeastern/numad23su_team_v2_group_10_final_project/RegisterActivity.java
+++ app/src/main/java/edu/northeastern/numad23su_team_v2_group_10_final_project/RegisterActivity.java
@@ -78,7 +78,7 @@ public class RegisterActivity extends AppCompatActivity {
 
         floatingActionButton.setOnClickListener(v -> {
             ImagePicker.with(this)
-                    .crop()	    			//Crop image(Optional), Check Customization for more option
+                    .crop()//Crop image(Optional), Check Customization for more option
                     .compress(1024)			//Final image size will be less than 1 MB(Optional)
                     .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
                     .start();

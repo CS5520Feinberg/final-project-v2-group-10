b61736b64be9a11b8fb16583725c09a86ab6ec16
Alan Hua <alanhua15@gmail.com>
2023-08-07 01:13:11 -0700

    update user posts layout

--- app/src/main/java/edu/northeastern/numad23su_team_v2_group_10_final_project/post/display_post/DisplayUserPostsActivity.java
+++ app/src/main/java/edu/northeastern/numad23su_team_v2_group_10_final_project/post/display_post/DisplayUserPostsActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
@@ -17,6 +18,8 @@ import android.widget.TextView;
 
 
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -25,12 +28,16 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 import edu.northeastern.numad23su_team_v2_group_10_final_project.R;
 import edu.northeastern.numad23su_team_v2_group_10_final_project.databinding.ActivityDisplayUserPostsBinding;
 import edu.northeastern.numad23su_team_v2_group_10_final_project.message.MessageFragment;
 import edu.northeastern.numad23su_team_v2_group_10_final_project.model.User;
 import edu.northeastern.numad23su_team_v2_group_10_final_project.product.ProductFragment;
 import edu.northeastern.numad23su_team_v2_group_10_final_project.profile.ProfileFragment;
+import edu.northeastern.numad23su_team_v2_group_10_final_project.public_fragments.FSAdapter;
+import edu.northeastern.numad23su_team_v2_group_10_final_project.public_fragments.TempPostFragment;
 import edu.northeastern.numad23su_team_v2_group_10_final_project.service.ServiceFragment;
 
 public class DisplayUserPostsActivity extends AppCompatActivity {
@@ -51,20 +58,39 @@ public class DisplayUserPostsActivity extends AppCompatActivity {
         binding = ActivityDisplayUserPostsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-       binding.bottomNavigationViewUserPosts.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+        TabLayout tabLayout = findViewById(R.id.tab_layout_user_posts);
+        ViewPager2 viewpager = findViewById(R.id.view_pager_user_posts);
+        FSAdapter vpAdapter = new FSAdapter(this);
+        vpAdapter.addFragment(TempPostFragment.newInstance("", 0, true));
+        vpAdapter.addFragment(TempPostFragment.newInstance("", 2, true));
+        vpAdapter.addFragment(TempPostFragment.newInstance("", 1, true));
+        vpAdapter.addFragment(TempPostFragment.newInstance("", 3, true));
+        viewpager.setAdapter(vpAdapter);
+        ArrayList<String> fragmentTitle = new ArrayList<>();
+        fragmentTitle.add("products");
+        fragmentTitle.add("services");
+        fragmentTitle.add("products in need");
+        fragmentTitle.add("services in need");
+        new TabLayoutMediator(tabLayout, viewpager,
+                (tab, position) -> tab.setText(fragmentTitle.get(position))
+        ).attach();
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-                if (id == R.id.product) {
-                    invalidateOptionsMenu();
-                    replaceFragment("product");
-                } else if (id == R.id.service) {
-                    invalidateOptionsMenu();
-                    replaceFragment("service");
-                }
-                return true;
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewpager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
             }
         });
+
 
         userPostsTitle = findViewById(R.id.user_posts_title);
 
@@ -100,45 +126,45 @@ public class DisplayUserPostsActivity extends AppCompatActivity {
             Log.d(TAG, "No current user.");
         }
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        Fragment productFragment = new ProductFragment();
-        Bundle args = new Bundle();
-        args.putBoolean("showCurrentUserPostsOnly", true);
-        productFragment.setArguments(args);
-        fragmentTransaction.add(R.id.frameLayoutUserPosts, productFragment, "product");
-        fragmentTransaction.show(productFragment).commit();
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//        Fragment productFragment = new ProductFragment();
+//        Bundle args = new Bundle();
+//        args.putBoolean("showCurrentUserPostsOnly", true);
+//        productFragment.setArguments(args);
+//        fragmentTransaction.add(R.id.frameLayoutUserPosts, productFragment, "product");
+//        fragmentTransaction.show(productFragment).commit();
 
     }
 
 
-    private Fragment checkFragment(String tag, FragmentManager fragmentManager, FragmentTransaction fragmentTransaction) {
-        Fragment fragment = fragmentManager.findFragmentByTag(tag);
-        if (fragment == null) {
-            if (tag.equals("product")) {
-                fragment = productFragment = new ProductFragment();
-                Bundle args = new Bundle();
-                args.putBoolean("showCurrentUserPostsOnly", true);
-                fragment.setArguments(args);
-                fragmentTransaction.add(R.id.frameLayoutUserPosts, fragment, "product");
-
-            } else if (tag.equals("service")) {
-                fragment = serviceFragment = new ServiceFragment();
-                Bundle args = new Bundle();
-                args.putBoolean("showCurrentUserPostsOnly", true);
-                fragment.setArguments(args);
-                fragmentTransaction.add(R.id.frameLayoutUserPosts, serviceFragment, "service");
-            }
-        }
-        return fragment;
-    }
-    private void replaceFragment(String tag) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        Fragment fragment = checkFragment(tag, fragmentManager, fragmentTransaction);
-        hideAllFragments(fragmentManager, fragmentTransaction);
-        fragmentTransaction.show(fragment).commit();
-    }
+//    private Fragment checkFragment(String tag, FragmentManager fragmentManager, FragmentTransaction fragmentTransaction) {
+//        Fragment fragment = fragmentManager.findFragmentByTag(tag);
+//        if (fragment == null) {
+//            if (tag.equals("product")) {
+//                fragment = productFragment = new ProductFragment();
+//                Bundle args = new Bundle();
+//                args.putBoolean("showCurrentUserPostsOnly", true);
+//                fragment.setArguments(args);
+//                fragmentTransaction.add(R.id.frameLayoutUserPosts, fragment, "product");
+//
+//            } else if (tag.equals("service")) {
+//                fragment = serviceFragment = new ServiceFragment();
+//                Bundle args = new Bundle();
+//                args.putBoolean("showCurrentUserPostsOnly", true);
+//                fragment.setArguments(args);
+//                fragmentTransaction.add(R.id.frameLayoutUserPosts, serviceFragment, "service");
+//            }
+//        }
+//        return fragment;
+//    }
+//    private void replaceFragment(String tag) {
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//        Fragment fragment = checkFragment(tag, fragmentManager, fragmentTransaction);
+//        hideAllFragments(fragmentManager, fragmentTransaction);
+//        fragmentTransaction.show(fragment).commit();
+//    }
 
     private void hideAllFragments(FragmentManager fragmentManager, FragmentTransaction transaction) {
         for (String tag: tags) {
@@ -149,11 +175,7 @@ public class DisplayUserPostsActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        outState.putInt("SEL", binding.bottomNavigationViewUserPosts.getSelectedItemId());
-        super.onSaveInstanceState(outState);
-    }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
